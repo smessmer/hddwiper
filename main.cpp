9e5dc2a3a8845493b7ba77658da6461c961c627f
Sebastian Messmer <mail@smessmer.de>
2011-05-06 18:16:47 +0000

    git-svn-id: https://svn.gwserver.de:3691/hddwiper/trunk@13 c257ac6a-9548-4c2e-97fb-1c9d84f8ff2e

--- main.cpp
+++ main.cpp
@@ -19,8 +19,8 @@ int main(int argc, char *argv[])
 		cout << "\rWritten: " << static_cast<double>(wiper.getBytesWritten())/1024/1024/1024 << " GB "
 			 << "Speed: " << wiper.getCurrentSpeed() << "MB/s "
 			 << "Seedbuffer: " << wiper.getSeedBufferSize() << " "
-			 << "Randombuffer: " << wiper.getBufferSize()
-			 << "Seeding: "<<wiper.getSeedingStatus()
+			 << "Randombuffer: " << wiper.getBufferSize() << " "
+			 << "Seeding: "<<wiper.getSeedingStatus() << "               "
 			 << flush;
 		sleep(1);
 	}
