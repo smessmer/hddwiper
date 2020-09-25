5dc52ecd0555ad093dd39f5044b521ea81c4a109
Sebastian Messmer <mail@smessmer.de>
2020-09-25 09:05:43 +0000

    Make Semaphore interruptible to make hddwiper shutdown correctly when the disk is full

--- util/thread/impl/Semaphore.impl.hpp
+++ util/thread/impl/Semaphore.impl.hpp
@@ -1,3 +1,6 @@
+#include <chrono>
+#include <boost/thread.hpp>
+
 inline Semaphore::Semaphore(int value)
 	:_mutex(),_wait(),_value(value)
 {
@@ -7,7 +10,12 @@ inline void Semaphore::wait()
 {
 	std::unique_lock<std::mutex> lock(_mutex);
 
-	_wait.wait(lock, [&] {return _value > 0;});
+        while (_value <= 0) {
+            _wait.wait_for(lock, std::chrono::milliseconds(100), [&] {
+                boost::this_thread::interruption_point();
+                return _value > 0;
+            });
+        }
 
 	--_value;
 }
