764147e3053e14c85900a16c4104de31d1cc4f7d
Sebastian Messmer <messmer@fb.com>
2022-04-27 15:32:10 -0700

    Show warnings by default

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,8 @@ struct Args {
 }
 
 fn main() -> Result<()> {
-    env_logger::init();
+    env_logger::init_from_env(
+        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"));
 
     let args = Args::parse();
 
